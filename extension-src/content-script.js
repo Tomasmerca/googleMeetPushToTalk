c2a403a68902a32f3393becbc42d94be727c5746
Tomasmerca <32470264+Tomasmerca@users.noreply.github.com>
2021-11-27 14:47:01 -0300

    Cambio para utilizar el boton Play/pausa

--- extension-src/content-script.js
+++ extension-src/content-script.js
@@ -1,13 +1,16 @@
 console.log('hi I\'m google meet extension')
-
+// "MediaPlayPause" 	Toggles between playing and pausing the current media. 	VK_MEDIA_PLAY_PAUSE (0xB3)
+//APPCOMMAND_MEDIA_PLAY_PAUSE
 // Buttons
+
+
 let micBtn, chatBar, videoBtn;
-let key = 18;
+let key = "MediaPlayPause";
 const addKeyPressEvent = () => {
     document.addEventListener('keydown', (e) => {
 
         micBtn = findMic()
-        if (e.keyCode == key && isMicOff()) {
+        if (e.key == key && isMicOff()) {
             micBtn.click()
         }
     });
